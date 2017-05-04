8d9043483be81b3a8dd641d624007c910d090541
nryn <narayanohanlon@gmail.com>
2017-05-04 10:01:34 +0100

    adding seeding logging

--- index.js
+++ index.js
@@ -3,10 +3,18 @@
 const torrentParser = require('./src/torrentParser');
 const Download = require ('./src/download')
 const torrent = torrentParser.open(process.argv[2]);
-const fileSize = require('fs').statSync("./" + torrent.info.name).size
+const fs = require('fs')
+let fileSize;
+
+try {
+  fileSize = fs.statSync("./" + torrent.info.name).size;
+} catch(e) {
+  fileSize = 0;
+}
 
 if (fileSize >= torrent.info.length) {
   const server = require('./src/server.js')
+  console.log("Seeding " + torrent.info.name + "...")
   server.startServer(torrent);
 } else {
   const download = new Download(torrent, torrent.info.name);
